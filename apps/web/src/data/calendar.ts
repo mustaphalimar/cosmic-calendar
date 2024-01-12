9907457ad0b8abca7e3d344e962a1b1c939cb822
mustaphalimar <tapham079@gmail.com>
2024-01-13 00:12:29 +0100

    updates

--- apps/web/src/data/calendar.ts
+++ apps/web/src/data/calendar.ts
@@ -1,62 +1,127 @@
+// const months = [
+//   {
+//     id: 1,
+//     name: "January",
+//     range: ["0", "1,172,054,795"],
+//   },
+//   {
+//     id: 2,
+//     name: "February",
+//     range: ["1,172,054,795", "2,230,684,931"],
+//   },
+//   {
+//     id: 3,
+//     name: "Mars",
+//     range: ["2,230,684,931", "3,402,739,726"],
+//   },
+//   {
+//     id: 4,
+//     name: "April",
+//     range: ["3,402,739,726", "4,536,986,301"],
+//   },
+//   {
+//     id: 5,
+//     name: "May",
+//     range: ["4,536,986,301", "5,709,041,096"],
+//   },
+//   {
+//     id: 6,
+//     name: "June",
+//     range: ["5,709,041,096", "6,843,287,671"],
+//   },
+//   {
+//     id: 7,
+//     name: "July",
+//     range: ["6,843,287,671", "8,015,342,466"],
+//   },
+//   {
+//     id: 8,
+//     name: "August",
+//     range: ["8,015,342,466", "9,187,397,260"],
+//   },
+//   {
+//     id: 9,
+//     name: "September",
+//     range: ["9,187,397,260", "10,321,643,836"],
+//   },
+//   {
+//     id: 10,
+//     name: "October",
+//     range: ["10,321,643,836", "11,493,698,630"],
+//   },
+//   {
+//     id: 11,
+//     name: "November",
+//     range: ["11,493,698,630", "12,627,945,205"],
+//   },
+//   {
+//     id: 12,
+//     name: "December",
+//     range: ["12,627,945,205", "13,800,000,000"],
+//   },
+// ];
+
 const months = [
   {
     id: 1,
     name: "January",
-    range: ["0", "1,172,054,795"],
+    range: ["The Big Bang", "1.15 Billion Years"],
   },
   {
     id: 2,
     name: "February",
-    range: ["1,172,054,795", "2,230,684,931"],
+    range: ["1.15", "2.3 Billion Years"],
   },
   {
     id: 3,
     name: "Mars",
-    range: ["2,230,684,931", "3,402,739,726"],
+    range: ["2.3", "3.45 Billion Years"],
   },
   {
     id: 4,
     name: "April",
-    range: ["3,402,739,726", "4,536,986,301"],
+    range: ["3.45", "4.6 Billion Years"],
   },
   {
     id: 5,
     name: "May",
-    range: ["4,536,986,301", "5,709,041,096"],
+    range: ["4.6", "5.75 Billion Years"],
   },
   {
     id: 6,
     name: "June",
-    range: ["5,709,041,096", "6,843,287,671"],
+    range: ["5.75", "6.9 Billion Years"],
   },
   {
     id: 7,
     name: "July",
-    range: ["6,843,287,671", "8,015,342,466"],
+    range: ["6.9", "8.05 Billion Years"],
   },
   {
     id: 8,
     name: "August",
-    range: ["8,015,342,466", "9,187,397,260"],
+    range: ["8.05", "9.2 Billion Years"],
   },
   {
     id: 9,
     name: "September",
-    range: ["9,187,397,260", "10,321,643,836"],
+    range: ["9.2", "10.35 Billion Years"],
   },
   {
     id: 10,
     name: "October",
-    range: ["10,321,643,836", "11,493,698,630"],
+    range: ["10.35", "11.5 Billion Years"],
   },
   {
     id: 11,
     name: "November",
-    range: ["11,493,698,630", "12,627,945,205"],
+    range: ["11.5", "12.65 Billion Years"],
   },
   {
     id: 12,
     name: "December",
-    range: ["12,627,945,205", "13,800,000,000"],
+    range: ["12.65", "The present day."],
   },
 ];
+
+export { months };
